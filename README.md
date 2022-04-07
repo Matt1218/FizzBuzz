4f44f0563e5197e01c31a0138d04133dbd22a311
Matt1218 <welfleym@gmail.com>
2022-04-07 12:36:03 -0400

    FizzBuzz!

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-FizzBuzz
+FizzBuzz for loop counting from 0 to 100
--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,17 @@
 public class FizzBuzz {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to FizzBuzz!");
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz!");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+        }
+    }
 }
